8c999eebb96936f55a212b66477698e084822cfc
Taylor Leong <imwhoiem@gmail.com>
2022-05-15 14:02:13 -0700

    added code for correct / wrong display

--- quiz_script.js
+++ quiz_script.js
@@ -1,19 +1,51 @@
-const question = document.getElementById('question')
-const answers = Array.from(document.getElementsByClassName('answer-container'))
+// get elements
+const question = document.getElementById('question');
+const answers = Array.from(document.getElementsByClassName('answer-but'));
+const next = document.getElementById('next-but');
 
-let currentQuestion = {}
-let ready_ans = false
-let score = 0
-let questionShown = 0
-let unanswered_quest = []
+// declare variables
+let currentQuestion = {};
+let ready_ans = false;
+let questionAnswered = false;
+let score = 0;
+let questionShown = 0;
+let unanswered_quest = [];
 let questions = [
     {
         question: 'What is 2 + 2?', 
         answers: [
-            {text: '4', correct: true},
+            {text: '6', correct: false},
             {text: '22', correct: false},
             {text: '45', correct: false},
+            {text: '4', correct: true},
+        ]
+    },
+    {
+        question: 'Inside which HTML element do we put the JavaScript??',
+        answers: [
+            {text: '<script>', correct: true},
+            {text: '<javascript>', correct: false},
+            {text: '<js>', correct: false},
+            {text: '<scripting>', correct: false},
+        ]
+    },
+    {
+        question: " How do you write 'Hello World' in an alert box?",
+        answers: [
+            {text: "msgBox('Hello World');", correct: false},
+            {text: "alertBox('Hello World';", correct: false},
+            {text: "msg('Hello World');", correct: false},
+            {text: "alert('Hello World');", correct: true},
+        ]
+
+    },
+    {
+        question: 'What is 2 + 2?', 
+        answers: [
             {text: '6', correct: false},
+            {text: '22', correct: false},
+            {text: '45', correct: false},
+            {text: '4', correct: true},
         ]
     },
     {
@@ -35,79 +67,98 @@ let questions = [
         ]
 
     }
-]
+];
 
-const CORRECT = 10
-const MAX_QUEST = 3
+// declare constant
+const CORRECT = 10;
+const MAX_QUEST = 6;
 
 function nextQuestion() {
+    // check if end quiz 
     if (unanswered_quest.length == 0 || questionShown >= MAX_QUEST)
-        return window.location.assign('/end.html')
+        return window.location.assign('/end.html');
     
-    /* show a random question */
-    const index = Math.floor(Math.random() * unanswered_quest.length)
-    currentQuestion = unanswered_quest[index]
-    question.innerText = currentQuestion.question
-    questionShown++
+    // show a random question 
+    const index = Math.floor(Math.random() * unanswered_quest.length);
+    currentQuestion = unanswered_quest[index];
+    question.innerText = currentQuestion.question;
+    
+    // set values for flags
+    questionAnswered = false;
+    questionShown++;
 
-    /* show the choices for the question */
+    // show the choices for the question 
     answers.forEach((ans) => {
-        ans.lastElementChild.innerText = currentQuestion.answers[ans.dataset['number']].text
-    })
+        ans.lastElementChild.innerText = currentQuestion.answers[ans.dataset['number']].text;
+    });
 
-    /* delete the shown question from the array */
-    unanswered_quest.splice(index, 1)
-    ready_ans = true
+    // delete the shown question from the array 
+    unanswered_quest.splice(index, 1);
+    ready_ans = true;
 
 }
 
-function addListenerToAnswers(ans) {
+function addListenerToAnswer(ans) {
     ans.addEventListener('click', (event) => {
+        // if question not shown, don't react
         if (!ready_ans)
-            return
-        ready_ans = false
+            return;
 
-        let selected_element = event.target
-        if (!selected_element.classList.contains('answer-container'))
-            selected_element = selected_element.parentElement
+        
+        let selected_element = event.target;
 
-        const selected_ans = selected_element.dataset['number']
-        if (currentQuestion.answers[selected_ans].correct){
-            event.target.classList.add('correct')
-            document.body.classList.add('correct')
-        } else {
-            event.target.classList.add('wrong')
-            document.body.classList.add('wrong')
-        }
-        nextQuestion()
-    })
+        // the user can click on the letters / button / text
+        // this assert we are dealing with the button
+        if (!selected_element.classList.contains('answer-but'))
+            selected_element = selected_element.parentElement;
+
+        // get the data-number of the button
+        const selected_ans = selected_element.dataset['number'];
+        let classToAdd = 'wrong'
+        if (currentQuestion.answers[selected_ans].correct)
+            classToAdd = 'correct'
+    
+        // change the theme 
+        selected_element.classList.add(classToAdd);
+        document.body.classList.add(classToAdd);
+
+        // set values for flags
+        questionAnswered = true;
+        ready_ans = false;
+    });
 } 
 
-function addListenerToNext(ans) {
-    ans.addEventListener('click', (event) => {
-        if (!ready_ans)
-            return
-        ready_ans = false
+function removeClass(element, cls) {
+    if (element.classList.contains(cls))
+        element.classList.remove(cls);
+}
 
-        const selected_ans = event.target.dataset['number']
-        if (currentQuestion.answers[selected_ans].correct){
-            event.target.classList.add('correct')
-            document.body.classList.add('correct')
-        } else {
-            event.target.classList.add('wrong')
-            document.body.classList.add('wrong')
-        }
-        nextQuestion()
-    })
+function eventForNext() {
+    // don't react if question is not answered
+    if (!questionAnswered) {
+        alert("Question Not Answered");
+        return;
+    }
+    
+    // remove themes
+    answers.forEach((ans) => {
+        removeClass(ans, 'correct');
+        removeClass(ans, 'wrong');
+    });
+    
+    removeClass(document.body, 'correct');
+    removeClass(document.body, 'wrong');
+
+    // show next question
+    nextQuestion();
 } 
 
 
 function startQuiz() {
-    questionShown = 0
-    score = 0
-    unanswered_quest = [...questions] /* spread opeator: make a copy of the array */
-    nextQuestion()
-    answers.forEach(addListenerToAnswers)
+    unanswered_quest = [...questions]; /* spread opeator: make a copy of the array */
+    nextQuestion();
+    answers.forEach(addListenerToAnswer);
+    next.addEventListener('click', eventForNext);
 }
 
-startQuiz()
+startQuiz();
