8dcf0f94fdbd444872918fa7d911f5452909cac0
Taylor Leong <imwhoiem@gmail.com>
2022-05-29 15:02:32 -0700

    added post and get for quiz route

--- routes/quiz.js
+++ routes/quiz.js
@@ -2,42 +2,41 @@ const express = require('express');
 const db = require('./database/mysql');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.render('quiz')
-});
-
-
-  /* GET for quiz/username/random_id (first in and next button click) */ 
-  router.get("/:user_id/:ques_id", async (req, res) => {
-    // get question with given id = random_id
-    let query = `select question from questions where id=${req.params.ques_id}}`;
-    let question = await execute_query(query);
-    // get choices for the corresponding question
-    query = `select choice, is_answer from question_choices where question_id=${req.params.ques_id}}`;
-    let choices = await execute_query(query);
-    // pass to client  
-    res.json({question: question, choices : choices});
-  })
-   
-
-  /* POST for  quiz/username/random_id (choices click) */
-  router.post("/:user_id/:ques_id", async (req, res) => {
-    // get the choice id from body (pass from client request)
-    // save the choice of the user to db
-    // get the new random_id (pass from client request)
-    // redirect to quiz/new_random_id
-    
-  })
+/* GET for quiz/user_id/random_id (first in and next button click) */ 
+router.get("/:user_id/:ques_id", async (req, res) => {
+  // get question with given id = random_id
+  let query = `select question from questions where id=${req.params.ques_id};`;
+  let question = await execute_query(query);
+  // get choices for the corresponding question
+  query = `select id, choice, is_answer from question_choices where question_id=${req.params.ques_id};`;
+  let choices = await execute_query(query);
+  // pass to client  
+  res.json({question: question, choices : choices});
+})
+ 
+/* POST for  quiz/user_id/random_id (choices click) */
+router.post("/:user_id/:ques_id", async (req, res) => {
+  // get the choice id  (pass from request query)
+  // save the choice of the user to db
+  let query = `insert into user_answers (user_id, question_id, choice_id) 
+               values(${req.params.user_id},${req.params.ques_id},${req.query.choice});`;
+  await execute_query(query);
+  query = `update user_answers set is_correct = 
+          (select is_answer from question_choices where question_id = ${req.params.ques_id} and id = ${req.query.choice})
+          where user_id = ${req.params.user_id} and question_id = ${req.params.ques_id} and choice_id = ${req.query.choice};`;
+  await execute_query(query);
+  // get the new random_id (pass from request query)
+  // redirect to quiz/user_id/new_random_id
+  res.redirect(`/${req.params.user_id}/${req.params.ques_id}`);
   
+})
 
 
-  function execute_query(query) {
-    let data = db.query(query, (err, result) => {
-                    if (err) throw err;
-                    return result;
-                });
-    return data;
-  }
-
+function execute_query(query) {
+  let data = db.query(query, (err, result) => {
+                  if (err) throw err;
+                  return result; });
+  return data;
+}
 
 module.exports = router;
